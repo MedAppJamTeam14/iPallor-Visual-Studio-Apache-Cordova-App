a8f4810110ef464d77fce572620ecfaa43e1b206
Max <maxpaulus43@gmail.com>
2015-11-09 20:40:32 -0800

    first commit

---
+++ IPallor_App/IPallor_App/www/scripts/controllers.js
@@ -0,0 +1,34 @@
+angular.module('app.controllers', [])
+  
+.controller('iPallorCtrl', function($scope) {
+
+})
+   
+.controller('patientInfoCtrl', function($scope) {
+
+})
+   
+.controller('diagnosisHistoryCtrl', function($scope) {
+
+})
+   
+.controller('dataPointsCtrl', function($scope) {
+
+})
+   
+.controller('diagnosisDetailsCtrl', function($scope) {
+
+})
+   
+.controller('uploadCtrl', function($scope) {
+
+})
+   
+.controller('addPatientCtrl', function($scope) {
+
+})
+   
+.controller('confirmSubmissionCtrl', function($scope) {
+
+})
+    
