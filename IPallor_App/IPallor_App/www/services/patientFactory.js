fdc07a89291d2b6af32585e43c453951bcef56d6
Pasha Khosravi <khosravipasha@gmail.com>
2015-11-15 20:55:54 -0800

    Adding point highlighting for diagnosis data.
    PROBABLY REALLY BUGGED

--- IPallor_App/IPallor_App/www/services/patientFactory.js
+++ IPallor_App/IPallor_App/www/services/patientFactory.js
@@ -13,25 +13,29 @@
           eye : 'left',
           date: '10/25/15',
           likelihood: 85,
-          src: ''
+          src: '',
+		  dataPoints: [{x:100 y:50},{x:10 y:150},{x:200 y:100},{x:150 y:100},{x:200 y:40}]
         },
         {
           eye: 'right',
           date: '9/14/15',
           likelihood: 80,
           src: ''
+		  dataPoints: [{x:10 y:200},{x:50 y:150},{x:150 y:100},{x:15 y:120},{x:180 y:40}]
         },
         {
           eye: 'left',
           date: '3/6/15',
           likelihood: 75,
           src: ''
+		  dataPoints: [{x:125 y:75},{x:100 y:10},{x:180 y:100},{x:150 y:120},{x:140 y:40}]
         },
         {
           eye: 'right',
           date: '11/25/14',
           likelihood: 70,
           src: ''
+		  dataPoints: [{x:75 y:20},{x:130 y:150},{x:100 y:140},{x:160 y:50},{x:20 y:110}]
         }
       ]
     },
