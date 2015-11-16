f799423d40a16fb021f2a01818fe3be74223b42e
Van Nguyen <vnguyen94@gmail.com>
2015-11-15 22:02:39 -0800

    rectangles for submit pic

--- IPallor_App/IPallor_App/www/views/processSubmit/processSubmit.controller.js
+++ IPallor_App/IPallor_App/www/views/processSubmit/processSubmit.controller.js
@@ -1,7 +1,9 @@
 ﻿angular.module('app')
-.controller('Process.SubmitCtrl', function ($location, PictureFactory) {
+.controller('Process.SubmitCtrl', function ($location, DiagnosisFactory, PictureFactory) {
 
   var vm = this;
+
+  var dataPoints = PictureFactory.getCurrentPicture().dataPoints;
 
   angular.extend(vm, {
     eye: 'Left',
@@ -18,5 +20,34 @@
       $location.path('/diagnosisDetails/details');
     }
   });
+
+  function init() {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext("2d");
+    var imageObj = new Image();
+    imageObj.onload = function () {
+      context.drawImage(imageObj, 0, 0, 300, 225);
+      drawPoints(dataPoints, 20);
+    };
+    imageObj.src = vm.src;
+  }
+
+  function drawPoints(points, size) {
+    for (var i = 0; i < points.length; ++i) {
+      var x = points[i].pos.x - size / 2.0;
+      var y = points[i].pos.y - size / 2.0;
+
+      context.clearRect(0, 0, context.width, context.height);
+      context.beginPath();
+      context.rect(x, y, size, size);
+      //context.fillStyle = "yellow";
+      //context.fill();
+      context.lineWidth = 2;
+      context.strokeStyle = 'black';
+      context.stroke();
+    }
+  }
+
+  init();
 
 });
